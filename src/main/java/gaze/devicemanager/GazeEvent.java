3df073272ec3d6860515f6d93423c2d5dc4437ac
Noars <55838787+Noars@users.noreply.github.com>
2023-09-20 16:06:42 +0200

    Fix folder & update Gaze java version

--- src/main/java/gaze/devicemanager/GazeEvent.java
+++ src/main/java/gaze/devicemanager/GazeEvent.java
@@ -1,9 +1,5 @@
 package gaze.devicemanager;
 
-/**
- * Created by schwab on 14/08/2016.
- */
-
 import javafx.event.EventTarget;
 import javafx.event.EventType;
 import javafx.scene.input.InputEvent;
@@ -17,10 +13,10 @@ public class GazeEvent extends InputEvent {
 
     public static final EventType<GazeEvent> GAZE_MOVED = new EventType<>(GazeEvent.ANY, "GAZE_MOVED");
 
-    public static final EventType<GazeEvent> GAZE_ENTERED_TARGET = new EventType<GazeEvent>(GazeEvent.ANY,
+    public static final EventType<GazeEvent> GAZE_ENTERED_TARGET = new EventType<>(GazeEvent.ANY,
             "GAZE_ENTERED_TARGET");
 
-    public static final EventType<GazeEvent> GAZE_EXITED_TARGET = new EventType<GazeEvent>(GazeEvent.ANY,
+    public static final EventType<GazeEvent> GAZE_EXITED_TARGET = new EventType<>(GazeEvent.ANY,
             "GAZE_EXITED_TARGET");
 
     public static final EventType<GazeEvent> GAZE_ENTERED = new EventType<>(GazeEvent.GAZE_ENTERED_TARGET,
